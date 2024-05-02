7ab35570f869bdcaec440ac9c39ec25f8a179074
Thércio Rodrigues <thercior@gmail.com>
2024-05-01 22:48:52 -0300

    :recycle: fix: adequando aos padrões da PEP08 (flake8)

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 import sys
-import os
 from download_youtube import YoutubeDownloader
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
